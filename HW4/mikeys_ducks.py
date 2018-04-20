1cdd30b3c60e437f6a3da4f37095d5fa39b273ff
johnjp15 <johnjp15@gmail.com>
2018-04-20 13:28:45 -0400

    duck

---
+++ HW4/mikeys_ducks.py
@@ -0,0 +1,27 @@
+TEAM_NAME = "mikeys_ducks" #Pick a team name
+MEMBERS = ["syc5pg", "jjp5nw", "ar9fh", "jn3qf"] #Include a list of your members’ UVA IDs
+
+
+'''
+# sample state format: 
+
+state = {
+"team-code": "eef8976e",
+"game": "sym",
+"opponent-name": "mighty-ducks",
+"prev-repetitions": 10, #Might be None if first game ever, or other number
+"last-opponent-play": 1, #0 or 1 depending on strategy played
+"last-outcome": 4, #Might be None if first game, or whatever outcome of play is
+"prospects": [
+[4,5],
+[3,2]
+]
+}
+
+'''
+
+
+def get_move(state):
+	pass
+
+###
