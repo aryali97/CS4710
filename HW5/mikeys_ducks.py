d7542791a10c066d732a2ea25d3cd4381916e7bd
Anirudh Ryali <aryali97@gmail.com>
2018-05-04 16:31:34 -0400

    Started adding shit

--- HW5/mikeys_ducks.py
+++ HW5/mikeys_ducks.py
@@ -3,9 +3,20 @@
 TEAM_NAME = "mikeys_ducks"
 MEMBERS = ["syc5pg", "jjp5nw", "ar9fh", "jn3qf"]
 
+info = {'utility': 1000000}
+
+
+
+def check_key(state, key):
+    if key not in state or state[key] == None:
+        return False
+    return True
+
+def get_bandit(state):
+    
+
 def main():
-    
-    
+        
     pass
 ###
 
