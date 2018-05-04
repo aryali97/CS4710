1c91637783f7d15c6105188ce3a34f7bb1f742e9
johnjp15 <johnjp15@gmail.com>
2018-05-04 16:26:31 -0400

    Wrote phase1, phase2, get_move methods

--- HW5/mikeys_ducks.py
+++ HW5/mikeys_ducks.py
@@ -3,13 +3,45 @@
 TEAM_NAME = "mikeys_ducks"
 MEMBERS = ["syc5pg", "jjp5nw", "ar9fh", "jn3qf"]
 
-def main():
-    
-    
+mikeys_ducks_info = {}
+
+'''
+{
+    "team-code": "eef8976e",
+    "game": "phase_1",
+    "pulls-left": 99999,
+    "last-cost": 0.75,
+    "last-payoff": 20.7,
+    "last-metadata": 00110101
+}
+'''
+
+def phase1(state): 
     pass
 ###
 
+def phase2(state):
+    pass
+###
 
+def get_move(state):
+    if(state["game"] == "phase_1"):
+        return phase1(state)
+    elif(state["game"] == "phase_2_a"):
+        # return phase2(state, 
+        # put what is needed for phase2 here
+        pass
+    elif(state["game"] == "phase_2_b"):
+        pass
+    else:
+        pass
+    #
+
+###
+
+def main():
+    pass
+###
 
 
 
